d748d4e3a75c387c3813a9206f5914eb9d5ef456
inhyeok Jeon <poiu2389@naver.com>
2022-05-31 02:05:48 +0000

    ui template

--- fastapi/server.py
+++ fastapi/server.py
@@ -4,8 +4,10 @@ from segmentation import get_segmentator, get_segments
 from starlette.responses import Response
 
 from fastapi import FastAPI, File
+from data import db_connect
 
-model = get_segmentator()
+# model = get_segmentator()
+cursor = db_connect()
 
 app = FastAPI(
     title="DeepLabV3 image segmentation",
@@ -15,10 +17,22 @@ app = FastAPI(
 )
 
 
-@app.post("/segmentation")
-def get_segmentation_map(file: bytes = File(...)):
-    """Get segmentation maps from image file"""
-    segmented_image = get_segments(model, file)
-    bytes_io = io.BytesIO()
-    segmented_image.save(bytes_io, format="PNG")
-    return Response(bytes_io.getvalue(), media_type="image/png")
+# @app.post("/segmentation")
+# def get_segmentation_map(file: bytes = File(...)):
+#     """Get segmentation maps from image file"""
+#     segmented_image = get_segments(model, file)
+#     bytes_io = io.BytesIO()
+#     segmented_image.save(bytes_io, format="PNG")
+#     return Response(bytes_io.getvalue(), media_type="image/png")
+
+@app.post("/{Token_ID}")
+def get_item_info(Token_ID: str):
+    cursor.execute(f"Select * from nftdb.TRADE where Token_ID = {Token_ID}")
+    test = cursor.fetchall()
+    return test
+
+@app.get("/{Token_ID}")
+def get_item_info(Token_ID: str):
+    cursor.execute(f"Select * from nftdb.TRADE where Token_ID = {Token_ID}")
+    test = cursor.fetchall()
+    return test
